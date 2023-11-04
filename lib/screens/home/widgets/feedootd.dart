3b8a2a5b0be1fed7e973785d248e423ee03d4482
« Christian » <christianbastide95@gmail.com>
2023-11-04 23:33:57 +0100

    Feed OK

--- lib/screens/home/widgets/feedootd.dart
+++ lib/screens/home/widgets/feedootd.dart
@@ -1,11 +1,10 @@
 // ignore_for_file: avoid_print
 
-import 'package:bootdv2/config/colors.dart';
+import 'package:bootdv2/cubits/liked_posts/liked_posts_cubit.dart';
 import 'package:bootdv2/screens/home/bloc/ootd/feed_ootd_bloc.dart';
-import 'package:bootdv2/widgets/cards/mosaique_event_large_card.dart';
+import 'package:bootdv2/screens/home/widgets/post_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 
 class FeedOOTD extends StatefulWidget {
   FeedOOTD({Key? key}) : super(key: key ?? GlobalKey());
@@ -21,24 +20,13 @@ class _FeedOOTDState extends State<FeedOOTD>
   final TextEditingController _textController = TextEditingController();
   bool _isFetching = false;
 
-  List<String> imageList = [
-    'assets/images/Stussy.png',
-    'assets/images/postImage2.jpg',
-    'assets/images/ITG1_2.jpg',
-    'assets/images/Carhartt.png',
-    'assets/images/Obey.png',
-    'assets/images/Sandro.png',
-  ];
-
   @override
   void initState() {
     super.initState();
-    print('FeedOOTD initState()');
     _scrollController = ScrollController()..addListener(_onScroll);
   }
 
   void _onScroll() {
-    print('FeedOOTD is scrolling');
     if (_scrollController.offset >=
             _scrollController.position.maxScrollExtent &&
         !_scrollController.position.outOfRange &&
@@ -52,7 +40,6 @@ class _FeedOOTDState extends State<FeedOOTD>
 
   @override
   void dispose() {
-    print('FeedOOTD dispose()');
     _textController.dispose();
     _scrollController.dispose();
     super.dispose();
@@ -60,8 +47,6 @@ class _FeedOOTDState extends State<FeedOOTD>
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-
     super.build(context);
     return BlocConsumer<FeedOOTDBloc, FeedOOTDState>(
       listener: (context, state) {
@@ -71,44 +56,66 @@ class _FeedOOTDState extends State<FeedOOTD>
       },
       builder: (context, state) {
         return Scaffold(
-          body: buildListview(size),
-          floatingActionButton: FloatingActionButton.extended(
-            onPressed: () => GoRouter.of(context).push('/event'),
-            label: Text(
-              "eventTest",
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineMedium!
-                  .copyWith(color: white),
-            ),
-            backgroundColor: couleurJauneOrange,
-          ),
+          body: _buildBody(state),
         );
       },
     );
   }
 
-  Widget buildListview(Size size) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 10.0),
-      child: SizedBox(
-        height: size.height * 0.2,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: imageList.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(right: 4),
-              child: MosaiqueEventLargeCard(
-                imageUrl: imageList[index],
-                title: 'Title',
-                description: 'Description',
+  Widget _buildBody(FeedOOTDState state) {
+    switch (state.status) {
+      case FeedOOTDStatus.loading:
+        return const Center(child: CircularProgressIndicator());
+      default:
+        return Stack(
+          children: [
+            ListView.separated(
+              physics: const BouncingScrollPhysics(),
+              cacheExtent: 10000,
+              controller: _scrollController,
+              itemCount: state.posts.length + 1,
+              separatorBuilder: (BuildContext context, int index) =>
+                  const SizedBox(height: 10),
+              itemBuilder: (BuildContext context, int index) {
+                // Si l'index est égal à la longueur des éléments, affichez un CircularProgressIndicator
+                // ou un SizedBox vide si la pagination n'est pas en cours
+                if (index == state.posts.length) {
+                  return state.status == FeedOOTDStatus.paginating
+                      ? const Center(child: CircularProgressIndicator())
+                      : const SizedBox.shrink();
+                } else {
+                  final post = state.posts[index];
+                  final likedPostsState =
+                      context.watch<LikedPostsCubit>().state;
+                  final isLiked =
+                      likedPostsState.likedPostIds.contains(post!.id);
+                  final recentlyLiked =
+                      likedPostsState.recentlyLikedPostIds.contains(post.id);
+                  return PostView(
+                    post: post,
+                    isLiked: isLiked,
+                    recentlyLiked: recentlyLiked,
+                    onLike: () {
+                      if (isLiked) {
+                        context.read<LikedPostsCubit>().unlikePost(post: post);
+                      } else {
+                        context.read<LikedPostsCubit>().likePost(post: post);
+                      }
+                    },
+                  );
+                }
+              },
+            ),
+            if (state.status == FeedOOTDStatus.paginating)
+              const Positioned(
+                bottom: 20,
+                left: 0,
+                right: 0,
+                child: Center(child: CircularProgressIndicator()),
               ),
-            );
-          },
-        ),
-      ),
-    );
+          ],
+        );
+    }
   }
 
 // Overridden to retain the state

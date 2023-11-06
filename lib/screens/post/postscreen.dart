9eca4f1126767d57db13f8c2d0e0ab448e925574
« Christian » <christianbastide95@gmail.com>
2023-11-06 22:38:29 +0100

    Fix

--- lib/screens/post/postscreen.dart
+++ lib/screens/post/postscreen.dart
@@ -12,10 +12,10 @@ class PostScreen extends StatelessWidget {
   final String username;
 
   const PostScreen({
-    Key? key,
+    super.key,
     required this.postId,
     required this.username,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {

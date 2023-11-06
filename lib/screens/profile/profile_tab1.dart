77d1a37e554e5d7d297c5f4493fbf5494c35888a
« Christian » <christianbastide95@gmail.com>
2023-11-06 22:35:39 +0100

    Fade animation

--- lib/screens/profile/profile_tab1.dart
+++ lib/screens/profile/profile_tab1.dart
@@ -1,9 +1,7 @@
+import 'package:bootdv2/config/configs.dart';
 import 'package:bootdv2/screens/profile/bloc/profile_bloc.dart';
 import 'package:bootdv2/widgets/cards/mosaique_profile_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shimmer/shimmer.dart';
-import 'dart:async'; // Importez dart:async pour utiliser Timer
 
 class ProfileTab1 extends StatefulWidget {
   final BuildContext context;
@@ -15,64 +13,31 @@ class ProfileTab1 extends StatefulWidget {
 }
 
 class _ProfileTab1State extends State<ProfileTab1> {
-  bool _isShimmering = true;
-
-  @override
-  void initState() {
-    super.initState();
-    // Commencer un timer pour l'effet shimmer
-    Timer(const Duration(milliseconds: 500), () {
-      if (mounted) {
-        setState(() {
-          _isShimmering = false;
-        });
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // Utilisez _isShimmering pour contrôler l'affichage
-    return _buildGridView(widget.context, widget.state, _isShimmering);
+    return _buildGridView(widget.context, widget.state);
   }
+}
 
-  Widget _buildGridView(
-      BuildContext context, ProfileState state, bool isShimmering) {
-    return Container(
-      color: Colors.white,
-      child: GridView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 5),
-        physics: const ClampingScrollPhysics(),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 6,
-          mainAxisSpacing: 6,
-          childAspectRatio: 0.8,
-        ),
-        itemCount: isShimmering ? 12 : state.posts.length,
-        itemBuilder: (context, index) {
-          if (isShimmering) {
-            // Afficher l'effet shimmer pour chaque élément
-            return Shimmer.fromColors(
-              baseColor: Colors.grey[300]!,
-              highlightColor: Colors.grey[100]!,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            );
-          } else {
-            // Afficher le contenu réel
-            final post = state.posts[index];
-            return MosaiqueProfileCard(
-              context,
-              imageUrl: post!.thumbnailUrl,
-            );
-          }
-        },
+Widget _buildGridView(BuildContext context, ProfileState state) {
+  return Container(
+    color: white,
+    child: GridView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 5),
+      physics: const ClampingScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 6,
+        mainAxisSpacing: 6,
+        childAspectRatio: 0.8,
       ),
-    );
-  }
+      itemCount: state.posts.length,
+      itemBuilder: (context, index) {
+        final post = state.posts[index];
+        return MosaiqueProfileCard(
+          imageUrl: post!.thumbnailUrl,
+        );
+      },
+    ),
+  );
 }
--- lib/widgets/cards/mosaique_profile_card.dart
+++ lib/widgets/cards/mosaique_profile_card.dart
@@ -1,34 +1,66 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
-class MosaiqueProfileCard extends StatelessWidget {
+class MosaiqueProfileCard extends StatefulWidget {
   final String imageUrl;
 
-  const MosaiqueProfileCard(
-    BuildContext context, {
-    super.key,
+  const MosaiqueProfileCard({
+    Key? key,
     required this.imageUrl,
-  });
+  }) : super(key: key);
+
+  @override
+  _MosaiqueProfileCardState createState() => _MosaiqueProfileCardState();
+}
+
+class _MosaiqueProfileCardState extends State<MosaiqueProfileCard> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _opacityAnimation;
+  bool _isImageLoaded = false; // Ajout d'un booléen pour suivre si l'image est chargée
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 200), // Durée de l'animation de fondu
+      vsync: this,
+    );
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          _isImageLoaded = true; // L'image est chargée une fois que l'animation est terminée
+        });
+      }
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return _buildCard(context, imageUrl);
-  }
-
-  Widget _buildCard(BuildContext context, String imageUrl) {
     Size size = MediaQuery.of(context).size;
-    return GestureDetector(
-      child: SizedBox(
-        height: size.height * 0.6,
-        width: size.width,
-        child: ClipRRect(
-          borderRadius:
-              BorderRadius.circular(10),
-          child: CachedNetworkImage(
-            fadeInDuration: const Duration(microseconds: 0),
-            fadeOutDuration: const Duration(microseconds: 0),
-            imageUrl: imageUrl,
-            fit: BoxFit.cover,
+    return FadeTransition(
+      opacity: _opacityAnimation,
+      child: GestureDetector(
+        child: SizedBox(
+          height: size.height * 0.6,
+          width: size.width,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: _isImageLoaded ? CachedNetworkImage( // L'image est chargée ici si _isImageLoaded est vrai
+              imageUrl: widget.imageUrl,
+              fit: BoxFit.cover,
+            ) : Container(color: Colors.transparent), // Un conteneur transparent est affiché pendant le fondu
           ),
         ),
       ),

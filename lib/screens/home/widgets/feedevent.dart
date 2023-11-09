7339c5961d307aa495c60dced4d231b7d0697692
« Christian » <christianbastide95@gmail.com>
2023-11-09 15:33:56 +0100

    Fix

--- lib/screens/home/widgets/feedevent.dart
+++ lib/screens/home/widgets/feedevent.dart
@@ -15,18 +15,15 @@ class _FeedCalendarState extends State<FeedCalendar>
   @override
   void initState() {
     super.initState();
-    // S'abonner aux événements dès que le widget est initialisé
     context.read<FeedEventBloc>().add(FeedEventFetchEvents());
   }
 
   @override
   Widget build(BuildContext context) {
-    super.build(
-        context); // Appel à super.build est nécessaire lorsque vous utilisez AutomaticKeepAliveClientMixin
+    super.build(context);
     return BlocBuilder<FeedEventBloc, FeedEventState>(
       builder: (context, state) {
         if (state.status == FeedEventStatus.loaded) {
-          // Assurez-vous d'utiliser state.events.length pour itemCount
           return Scaffold(
             body: GridView.builder(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -35,8 +32,7 @@ class _FeedCalendarState extends State<FeedCalendar>
                 mainAxisSpacing: 4,
                 childAspectRatio: 0.5,
               ),
-              itemCount:
-                  state.posts.length, // Utilisez state.events.length ici
+              itemCount: state.posts.length,
               itemBuilder: (context, index) {
                 final event = state.posts[index];
                 return event != null
@@ -44,8 +40,7 @@ class _FeedCalendarState extends State<FeedCalendar>
                         context,
                         imageUrl: event.imageUrl,
                         title: event.title,
-                        logoUrl: event.author
-                            .logoUrl, // Assurez-vous que cette propriété existe dans votre modèle Event
+                        logoUrl: event.author.logoUrl,
                       )
                     : const SizedBox.shrink();
               },
@@ -54,14 +49,12 @@ class _FeedCalendarState extends State<FeedCalendar>
         } else if (state.status == FeedEventStatus.loading) {
           return const Center(child: CircularProgressIndicator());
         } else {
-          // Gérer les cas d'erreur
           return Center(child: Text(state.failure.message));
         }
       },
     );
   }
 
-  // Override pour indiquer que vous voulez conserver l'état du widget
   @override
   bool get wantKeepAlive => true;
 }

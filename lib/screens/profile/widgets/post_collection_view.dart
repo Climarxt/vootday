25cd00514b02194684c4ba9ebd2658fe6e86bc14
« Christian » <christianbastide95@gmail.com>
2023-12-10 00:25:13 +0100

    Fix - Transition

--- lib/screens/profile/widgets/post_collection_view.dart
+++ lib/screens/profile/widgets/post_collection_view.dart
@@ -7,7 +7,6 @@ import 'package:bootdv2/screens/home/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-
 class PostCollectionView extends StatefulWidget {
   final Post post;
   final String collectionId;
@@ -26,33 +25,54 @@ class PostCollectionView extends StatefulWidget {
 
 class _PostCollectionViewState extends State<PostCollectionView>
     with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
   @override
   void initState() {
     super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeIn,
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => _navigateToPostScreen(context),
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(18),
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(18),
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 1.5,
-            child: Stack(
-              children: [
-                ImageLoaderPostEvent(
-                  imageProvider: widget.post.imageProvider,
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height / 1.5,
-                ),
-                buildBody(context),
-              ],
+    return FadeTransition(
+      opacity: _animation,
+      child: GestureDetector(
+        onTap: () => _navigateToPostScreen(context),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(18),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.5,
+              child: Stack(
+                children: [
+                  ImageLoaderPostEvent(
+                    imageProvider: widget.post.imageProvider,
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height / 1.5,
+                  ),
+                  buildBody(context),
+                ],
+              ),
             ),
           ),
         ),
@@ -65,19 +85,17 @@ class _PostCollectionViewState extends State<PostCollectionView>
         Uri.encodeComponent(widget.post.author.username);
     final String encodedTitle = Uri.encodeComponent(widget.title);
 
-    GoRouter.of(context).push(
-      '/home/event/${widget.collectionId}/post/${widget.post.id}'
-      '?username=$encodedUsername'
-      '&title=$encodedTitle'
-    );
+    GoRouter.of(context)
+        .push('/home/event/${widget.collectionId}/post/${widget.post.id}'
+            '?username=$encodedUsername'
+            '&title=$encodedTitle');
   }
 
   void _navigateToUserScreen(BuildContext context) {
-    GoRouter.of(context).push(
-      '/home/event/${widget.collectionId}/user/${widget.post.author.id}'
-      '?username=${Uri.encodeComponent(widget.post.author.username)}'
-      '&title=${Uri.encodeComponent(widget.title)}'
-    );
+    GoRouter.of(context)
+        .push('/home/event/${widget.collectionId}/user/${widget.post.author.id}'
+            '?username=${Uri.encodeComponent(widget.post.author.username)}'
+            '&title=${Uri.encodeComponent(widget.title)}');
   }
 
   Stack buildBody(BuildContext context) {

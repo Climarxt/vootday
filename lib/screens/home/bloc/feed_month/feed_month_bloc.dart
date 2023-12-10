0f3cf1ddfa013a477840856107f591ad47d08507
« Christian » <christianbastide95@gmail.com>
2023-12-10 22:14:06 +0100

    Clean code

--- lib/screens/home/bloc/feed_month/feed_month_bloc.dart
+++ lib/screens/home/bloc/feed_month/feed_month_bloc.dart
@@ -21,8 +21,6 @@ class FeedMonthBloc extends Bloc<FeedMonthEvent, FeedMonthState> {
         _authBloc = authBloc,
         super(FeedMonthState.initial()) {
     on<FeedMonthFetchPostsMonth>(_mapFeedMonthFetchPostsMonth);
-    on<FeedMonthFetchPosts>(_mapFeedMonthFetchPostsToState);
-    on<FeedMonthPaginatePosts>(_mapFeedMonthPaginatePostsToState);
   }
 
   Future<void> _mapFeedMonthFetchPostsMonth(
@@ -57,51 +55,5 @@ class FeedMonthBloc extends Bloc<FeedMonthEvent, FeedMonthState> {
     }
     debugPrint(
         '_mapFeedMonthFetchPostsMonth : Fin de _mapFeedMonthFetchPostsMonth');
-  }
-
-  Future<void> _mapFeedMonthFetchPostsToState(
-    FeedMonthFetchPosts event,
-    Emitter<FeedMonthState> emit,
-  ) async {
-    try {
-      final posts =
-          await _feedRepository.getFeedMonth(userId: _authBloc.state.user!.uid);
-
-      emit(
-        state.copyWith(posts: posts, status: FeedMonthStatus.loaded),
-      );
-    } catch (err) {
-      emit(state.copyWith(
-        status: FeedMonthStatus.error,
-        failure:
-            const Failure(message: 'Nous n\'avons pas pu charger votre flux'),
-      ));
-    }
-  }
-
-  Future<void> _mapFeedMonthPaginatePostsToState(
-    FeedMonthPaginatePosts event,
-    Emitter<FeedMonthState> emit,
-  ) async {
-    emit(state.copyWith(status: FeedMonthStatus.paginating));
-    try {
-      final lastPostId = state.posts.isNotEmpty ? state.posts.last!.id : null;
-
-      final posts = await _feedRepository.getFeedMonth(
-        userId: _authBloc.state.user!.uid,
-        lastPostId: lastPostId,
-      );
-      final updatedPosts = List<Post?>.from(state.posts)..addAll(posts);
-
-      emit(
-        state.copyWith(posts: updatedPosts, status: FeedMonthStatus.loaded),
-      );
-    } catch (err) {
-      emit(state.copyWith(
-        status: FeedMonthStatus.error,
-        failure: const Failure(
-            message: 'Quelque chose s\'est mal passé ! Veuillez réessayer.'),
-      ));
-    }
   }
 }

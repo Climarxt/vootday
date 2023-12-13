f6951250579ad76ba2e5cc56b2c9ebe939c0e2de
« Christian » <christianbastide95@gmail.com>
2023-12-13 13:43:47 +0100

    Fix

--- lib/screens/post/posts.dart
+++ lib/screens/post/posts.dart
@@ -1,6 +1,1 @@
-export 'post_calendar_screen.dart';
-export 'post_event_screen.dart';
-export 'post_home_screen.dart';
-export 'post_profile_screen.dart';
 export 'post_screen.dart';
-export 'post_search_screen.dart';

f033f06a6f32c296d211ea614b6322905dac4214
« Christian » <christianbastide95@gmail.com>
2023-12-17 14:42:01 +0100

    Add - like on postview

--- lib/screens/home/widgets/post_view.dart
+++ lib/screens/home/widgets/post_view.dart
@@ -1,11 +1,16 @@
 import 'dart:ui';
 
+import 'package:bootdv2/blocs/auth/auth_bloc.dart';
 import 'package:bootdv2/config/configs.dart';
+import 'package:bootdv2/cubits/add_post_to_likes/add_post_to_likes_cubit.dart';
 import 'package:bootdv2/models/models.dart';
 import 'package:bootdv2/screens/home/widgets/widgets.dart';
+import 'package:bootdv2/screens/profile/bloc/mycollection/mycollection_bloc.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class PostView extends StatefulWidget {
@@ -30,6 +35,9 @@ class PostView extends StatefulWidget {
 class _PostViewState extends State<PostView>
     with SingleTickerProviderStateMixin {
   bool isImageVisible = false;
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  List<String> _imageUrls = [];
 
   @override
   void initState() {
@@ -39,6 +47,27 @@ class _PostViewState extends State<PostView>
         setState(() {
           isImageVisible = true;
         });
+      }
+    });
+
+    final state = context.read<MyCollectionBloc>().state;
+    final nonNullCollections = state.collections
+        .where((collection) => collection != null)
+        .cast<Collection>()
+        .toList();
+    _fetchImageUrls(nonNullCollections);
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 1, end: 1.5)
+        .chain(CurveTween(curve: Curves.elasticOut))
+        .animate(_controller);
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
       }
     });
   }
@@ -123,7 +152,56 @@ class _PostViewState extends State<PostView>
           left: 12,
           child: buildUsername(context),
         ),
+        Positioned(
+          bottom: 14,
+          right: 16,
+          child: widget.post.id != null
+              ? buildFavoriteButton(
+                  context,
+                  widget.post
+                      .id!, // Utilisation de l'opérateur '!' pour indiquer que la valeur n'est pas 'null'
+                  _animation, // Assurez-vous que ces paramètres sont disponibles
+                  _controller,
+                )
+              : SizedBox
+                  .shrink(), // Ou un autre widget de remplacement si 'id' est 'null'
+        ),
       ],
+    );
+  }
+
+  Widget buildFavoriteButton(BuildContext context, String postId,
+      Animation<double> animation, AnimationController controller) {
+    return BlocBuilder<AddPostToLikesCubit, AddPostToLikesState>(
+      builder: (context, state) {
+        final cubit = context.read<AddPostToLikesCubit>();
+        final authState = context.read<AuthBloc>().state;
+        final userId = authState.user?.uid;
+
+        return FutureBuilder<bool>(
+          future: cubit.isPostLiked(postId, userId!),
+          builder: (context, snapshot) {
+            bool isLiked = snapshot.data ?? false;
+
+            return ScaleTransition(
+              scale: animation,
+              child: IconButton(
+                icon: Icon(isLiked ? Icons.favorite : Icons.favorite_border,
+                    color: white, size: 30),
+                onPressed: () async {
+                  controller.forward(from: 0.0);
+                  if (isLiked) {
+                    await cubit.deletePostRefFromLikes(
+                        postId: postId, userId: userId);
+                  } else {
+                    await cubit.addPostToLikes(postId, userId);
+                  }
+                },
+              ),
+            );
+          },
+        );
+      },
     );
   }
 
@@ -171,5 +249,71 @@ class _PostViewState extends State<PostView>
   void _navigateToPostScreen(BuildContext context) {
     final username = widget.post.author.username;
     GoRouter.of(context).push('/post/${widget.post.id}?username=$username');
+  }
+
+  Future<void> _fetchImageUrls(List<Collection> collections) async {
+    // Fetch all image URLs
+    List<String> urls = [];
+    for (var collection in collections) {
+      String imageUrl = await getMostRecentPostImageUrl(collection.id);
+      urls.add(imageUrl);
+    }
+
+    // Check if the state is still mounted before updating
+    if (mounted) {
+      setState(() {
+        _imageUrls = urls;
+      });
+    }
+  }
+
+  Future<String> getMostRecentPostImageUrl(String collectionId) async {
+    // Add a debug print to confirm the method is called with a valid ID
+    debugPrint(
+        "getMostRecentPostImageUrl : Fetching image URL for collection ID: $collectionId");
+
+    try {
+      final feedEventRef = FirebaseFirestore.instance
+          .collection('collections')
+          .doc(collectionId)
+          .collection('feed_collection');
+
+      final querySnapshot =
+          await feedEventRef.orderBy('date', descending: true).limit(1).get();
+
+      // Check if there are documents returned
+      if (querySnapshot.docs.isNotEmpty) {
+        final data = querySnapshot.docs.first.data();
+        final DocumentReference? postRef =
+            data['post_ref'] as DocumentReference?;
+
+        if (postRef != null) {
+          final postDoc = await postRef.get();
+
+          if (postDoc.exists) {
+            final postData = postDoc.data() as Map<String, dynamic>?;
+            final imageUrl = postData?['imageUrl'] as String? ?? '';
+            // Print the image URL to verify it's the correct one
+            debugPrint(
+                "getMostRecentPostImageUrl : Found image URL: $imageUrl");
+            return imageUrl;
+          } else {
+            debugPrint(
+                "getMostRecentPostImageUrl : Referenced post document does not exist.");
+          }
+        } else {
+          debugPrint("getMostRecentPostImageUrl : Post reference is null.");
+        }
+      } else {
+        debugPrint(
+            "getMostRecentPostImageUrl : No posts found in the collection's feed.");
+      }
+    } catch (e) {
+      // Print any exceptions that occur
+      debugPrint(
+          "getMostRecentPostImageUrl : An error occurred while fetching the post image URL: $e");
+    }
+    // Return a default image URL if no image is found or an error occurs
+    return 'https://firebasestorage.googleapis.com/v0/b/bootdv2.appspot.com/o/images%2Fbrands%2Fwhite_placeholder.png?alt=media&token=2d4e4176-e9a6-41e4-93dc-92cd7f257ea7';
   }
 }

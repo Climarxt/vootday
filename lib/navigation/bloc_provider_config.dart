a4ea4708383529d28c1c0065400d0a7e67a9f478
« Christian » <christianbastide95@gmail.com>
2023-12-20 10:26:32 +0100

    Fix

--- lib/navigation/bloc_provider_config.dart
+++ lib/navigation/bloc_provider_config.dart
@@ -49,6 +49,91 @@ class BlocProviderConfig {
     );
   }
 
+  static MultiBlocProvider getCalendarMultiBlocProvider(
+      BuildContext context, Widget child) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<CalendarLatestBloc>(
+          create: (context) {
+            final latestEventBloc = CalendarLatestBloc(
+              eventRepository: context.read<EventRepository>(),
+              authBloc: context.read<AuthBloc>(),
+            );
+            return latestEventBloc;
+          },
+        ),
+        BlocProvider(
+          create: (context) {
+            final thisWeekEventsBloc = CalendarThisWeekBloc(
+              eventRepository: context.read<EventRepository>(),
+              authBloc: context.read<AuthBloc>(),
+            );
+            return thisWeekEventsBloc;
+          },
+        ),
+        BlocProvider(
+          create: (context) {
+            final thisComignSoonEventsBloc = CalendarComingSoonBloc(
+              eventRepository: context.read<EventRepository>(),
+              authBloc: context.read<AuthBloc>(),
+            );
+            return thisComignSoonEventsBloc;
+          },
+        ),
+      ],
+      child: child,
+    );
+  }
+
+  static MultiBlocProvider getFollowingExplorerMultiBlocProvider(
+      BuildContext context, Widget child) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<FollowingBloc>(
+          create: (context) {
+            final followingBloc = FollowingBloc(
+              feedRepository: context.read<FeedRepository>(),
+              authBloc: context.read<AuthBloc>(),
+            );
+            return followingBloc;
+          },
+        ),
+        BlocProvider<ExplorerBloc>(
+          create: (context) {
+            final explorerBloc = ExplorerBloc(
+              feedRepository: context.read<FeedRepository>(),
+              authBloc: context.read<AuthBloc>(),
+            );
+            return explorerBloc;
+          },
+        ),
+      ],
+      child: child,
+    );
+  }
+
+  static MultiBlocProvider getMyProfileMultiBlocProvider(
+      BuildContext context, Widget child) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ProfileBloc>(
+          create: (context) => ProfileBloc(
+            authBloc: context.read<AuthBloc>(),
+            userRepository: context.read<UserRepository>(),
+            postRepository: context.read<PostRepository>(),
+          ),
+        ),
+        BlocProvider<MyCollectionBloc>(
+          create: (context) => MyCollectionBloc(
+            authBloc: context.read<AuthBloc>(),
+            postRepository: context.read<PostRepository>(),
+          ),
+        ),
+      ],
+      child: child,
+    );
+  }
+
   static MultiBlocProvider getProfileMultiBlocProvider(
       BuildContext context, Widget child) {
     return MultiBlocProvider(
@@ -103,42 +188,6 @@ class BlocProviderConfig {
     );
   }
 
-  static MultiBlocProvider getCalendarMultiBlocProvider(
-      BuildContext context, Widget child) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<CalendarLatestBloc>(
-          create: (context) {
-            final latestEventBloc = CalendarLatestBloc(
-              eventRepository: context.read<EventRepository>(),
-              authBloc: context.read<AuthBloc>(),
-            );
-            return latestEventBloc;
-          },
-        ),
-        BlocProvider(
-          create: (context) {
-            final thisWeekEventsBloc = CalendarThisWeekBloc(
-              eventRepository: context.read<EventRepository>(),
-              authBloc: context.read<AuthBloc>(),
-            );
-            return thisWeekEventsBloc;
-          },
-        ),
-        BlocProvider(
-          create: (context) {
-            final thisComignSoonEventsBloc = CalendarComingSoonBloc(
-              eventRepository: context.read<EventRepository>(),
-              authBloc: context.read<AuthBloc>(),
-            );
-            return thisComignSoonEventsBloc;
-          },
-        ),
-      ],
-      child: child,
-    );
-  }
-
   static BlocProvider getCreatePostBlocProvider(
       BuildContext context, Widget child) {
     return BlocProvider<CreatePostCubit>(
@@ -151,55 +200,6 @@ class BlocProviderConfig {
         );
         return createPostBloc;
       },
-      child: child,
-    );
-  }
-
-  static MultiBlocProvider getFollowingExplorerMultiBlocProvider(
-      BuildContext context, Widget child) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<FollowingBloc>(
-          create: (context) {
-            final followingBloc = FollowingBloc(
-              feedRepository: context.read<FeedRepository>(),
-              authBloc: context.read<AuthBloc>(),
-            );
-            return followingBloc;
-          },
-        ),
-        BlocProvider<ExplorerBloc>(
-          create: (context) {
-            final explorerBloc = ExplorerBloc(
-              feedRepository: context.read<FeedRepository>(),
-              authBloc: context.read<AuthBloc>(),
-            );
-            return explorerBloc;
-          },
-        ),
-      ],
-      child: child,
-    );
-  }
-
-  static MultiBlocProvider getMyProfileMultiBlocProvider(
-      BuildContext context, Widget child) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<ProfileBloc>(
-          create: (context) => ProfileBloc(
-            authBloc: context.read<AuthBloc>(),
-            userRepository: context.read<UserRepository>(),
-            postRepository: context.read<PostRepository>(),
-          ),
-        ),
-        BlocProvider<MyCollectionBloc>(
-          create: (context) => MyCollectionBloc(
-            authBloc: context.read<AuthBloc>(),
-            postRepository: context.read<PostRepository>(),
-          ),
-        ),
-      ],
       child: child,
     );
   }

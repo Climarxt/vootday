bf058b2520b6033f3367c3e6c691efebc9166f0e
« Christian » <christianbastide95@gmail.com>
2024-05-04 20:40:09 +0200

    Update Swipe Event

--- lib/screens/swipe/widgets/swipeevent.dart
+++ lib/screens/swipe/widgets/swipeevent.dart
@@ -1,7 +1,14 @@
+// ignore_for_file: unused_field
+
 import 'package:bootdv2/config/configs.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:bootdv2/models/models.dart';
+import 'package:bootdv2/screens/swipe/bloc/swipe_bloc.dart';
+import 'package:bootdv2/screens/swipe/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bounceable/flutter_bounceable.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
+import 'package:go_router/go_router.dart';
 
 class SwipeEvent extends StatefulWidget {
   const SwipeEvent({super.key});
@@ -10,138 +17,271 @@ class SwipeEvent extends StatefulWidget {
   State<SwipeEvent> createState() => _SwipeEventState();
 }
 
-class _SwipeEventState extends State<SwipeEvent> {
+class _SwipeEventState extends State<SwipeEvent>
+    with TickerProviderStateMixin, AutomaticKeepAliveClientMixin<SwipeEvent> {
   final CardSwiperController controller1 = CardSwiperController();
   final CardSwiperController controller2 = CardSwiperController();
-  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
-  List<String> _imageUrls1 = [];
-  List<String> _imageUrls2 = [];
-  bool _isLoading = true;
+  late AnimationController _heartAnimationController;
+  late Animation<double> _heartAnimation;
+  final List<String> _imageUrls1 = [];
+  final List<String> _imageUrls2 = [];
+  List<Post> _posts1 = [];
+  List<Post> _posts2 = [];
+  int _currentIndex1 = 0;
+  int _currentIndex2 = 0;
+
+  EdgeInsets _swiperPaddingLeft() => const EdgeInsets.only(
+        left: 0,
+        right: 7.5,
+        bottom: 7.5,
+        top: 0,
+      );
+
+  EdgeInsets _swiperPaddingRight() => const EdgeInsets.only(
+        left: 7.5,
+        right: 0,
+        bottom: 7.5,
+        top: 0,
+      );
+
+  AllowedSwipeDirection _allowedSwipeDirection() => AllowedSwipeDirection.only(
+      up: true, left: false, down: true, right: false);
+  late double verticalThresholdPercentageSave;
+  bool _hasShownBottomSheet = false;
 
   @override
   void initState() {
     super.initState();
-    _loadImages();
+    context.read<SwipeBloc>().add(SwipeFetchPostsOOTD());
+    _heartAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    _heartAnimation = Tween(begin: 65.0, end: 85.0).animate(
+      CurvedAnimation(
+        parent: _heartAnimationController,
+        curve: Curves.ease,
+      ),
+    );
   }
-
-  Future<void> _loadImages() async {
-    try {
-      // Récupérer tous les documents de la collection 'posts'
-      var postsSnapshot =
-          await _firebaseFirestore.collection(Paths.posts).get();
-
-      // Réinitialiser les listes pour éviter les doublons lors du rechargement
-      _imageUrls1.clear();
-      _imageUrls2.clear();
-
-      // Parcourir chaque document et répartir les URLs d'images entre _imageUrls1 et _imageUrls2
-      bool addToFirstList = true;
-      for (var doc in postsSnapshot.docs) {
-        var imageUrl = doc.data()["imageUrl"] as String?;
-        if (imageUrl != null) {
-          if (addToFirstList) {
-            _imageUrls1.add(imageUrl);
-          } else {
-            _imageUrls2.add(imageUrl);
-          }
-          addToFirstList = !addToFirstList; // Alterner entre les listes
-          print('Image URL ajoutée: $imageUrl');
-        }
-      }
-
-      print('Nombre total d\'images dans _imageUrls1: ${_imageUrls1.length}');
-      print('Nombre total d\'images dans _imageUrls2: ${_imageUrls2.length}');
-
-      setState(() {
-        _isLoading = false;
-      });
-    } catch (e) {
-      print('Erreur lors du chargement des images: $e');
-    }
-  }
-
-  int _currentIndex1 = 0;
-  int _currentIndex2 = 0;
 
   @override
   Widget build(BuildContext context) {
-    if (_isLoading) {
-      // Afficher un écran de chargement ou un conteneur blanc pendant le chargement
-      return Scaffold(
-        body: Center(
-          child:
-              CircularProgressIndicator(), // Vous pouvez remplacer cela par un conteneur blanc ou un autre widget si vous préférez
-        ),
-      );
+    super.build(context);
+
+    return BlocConsumer<SwipeBloc, SwipeState>(
+      listener: (context, state) {
+        // debugPrint("DEBUG : state : $state - context: $context ");
+        if (state.status == SwipeStatus.loaded) {
+          _loadImages(state.posts);
+        }
+      },
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Scaffold(
+            body: _buildBody(state),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildBody(SwipeState state) {
+    switch (state.status) {
+      case SwipeStatus.loading:
+        return const Center(child: CircularProgressIndicator());
+      case SwipeStatus.loaded:
+        return Scaffold(
+          body: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 0.29,
+                    child: CardSwiper(
+                      numberOfCardsDisplayed: 1,
+                      allowedSwipeDirection: _allowedSwipeDirection(),
+                      padding: _swiperPaddingLeft(),
+                      cardBuilder: (context,
+                              index,
+                              horizontalThresholdPercentage,
+                              verticalThresholdPercentage) =>
+                          _buildCard(
+                              _imageUrls1[_currentIndex1],
+                              verticalThresholdPercentage.toDouble(),
+                              _posts1[_currentIndex1]),
+                      cardsCount: _imageUrls1.length,
+                      controller: controller1,
+                      onSwipe: (previousIndex, currentIndex, direction) {
+                        if (direction == CardSwiperDirection.top ||
+                            direction == CardSwiperDirection.bottom) {
+                          _changeImage(1);
+                          _changeImage(2);
+                        }
+                        return true;
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 0.29,
+                    child: CardSwiper(
+                      numberOfCardsDisplayed: 1,
+                      allowedSwipeDirection: _allowedSwipeDirection(),
+                      padding: _swiperPaddingRight(),
+                      cardBuilder: (context,
+                              index,
+                              horizontalThresholdPercentage,
+                              verticalThresholdPercentage) =>
+                          _buildCard(
+                              _imageUrls2[_currentIndex2],
+                              verticalThresholdPercentage.toDouble(),
+                              _posts2[_currentIndex2]),
+                      cardsCount: _imageUrls2.length,
+                      controller: controller2,
+                      onSwipe: (previousIndex, currentIndex, direction) {
+                        debugPrint(
+                            "previousIndex: $previousIndex, currentIndex: $currentIndex, direction: $direction ");
+                        if (direction == CardSwiperDirection.top ||
+                            direction == CardSwiperDirection.bottom) {
+                          _changeImage(1);
+                          _changeImage(2);
+                        }
+
+                        return true;
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      default:
+        return Container(color: white);
     }
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: AspectRatio(
-                aspectRatio: 1.22,
-                child: CardSwiper(
-                  numberOfCardsDisplayed: 1,
-                  padding:
-                      const EdgeInsets.only(right: 10, left: 10, bottom: 5),
-                  cardBuilder: (context, index, _, __) =>
-                      _buildCard(_imageUrls1[_currentIndex1]),
-                  cardsCount: _imageUrls1.length,
-                  controller: controller1,
-                  onSwipe: (previousIndex, currentIndex, direction) {
-                    _changeImage(1);
-                    _changeImage(2);
-                    return true;
-                  },
-                ),
+  }
+
+  Widget _buildCard(
+      String imageUrl, double verticalSwipePercentage, Post post) {
+    bool shouldAnimateUp = verticalSwipePercentage < -150;
+    bool shouldAnimatetDown = verticalSwipePercentage > 150;
+    // debugPrint("DEBUG : post: $post");
+
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (shouldAnimateUp || shouldAnimatetDown) {
+        _heartAnimationController.repeat(reverse: true);
+        ;
+      } else {
+        _heartAnimationController.reset();
+      }
+    });
+
+    return Bounceable(
+      onTap: () => _navigateToPostScreen(context, post),
+      child: Stack(
+        children: [
+          // Image
+          Positioned.fill(
+            child: Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
               ),
             ),
-            Expanded(
-              child: AspectRatio(
-                aspectRatio: 1.22,
-                child: CardSwiper(
-                  numberOfCardsDisplayed: 1,
-                  padding: const EdgeInsets.only(
-                      right: 10, left: 10, top: 5, bottom: 5),
-                  cardBuilder: (context, index, _, __) =>
-                      _buildCard(_imageUrls2[_currentIndex2]),
-                  cardsCount: _imageUrls2.length,
-                  controller: controller2,
-                  onSwipe: (previousIndex, currentIndex, direction) {
-                    _changeImage(1);
-                    _changeImage(2);
-                    return true;
-                  },
-                ),
+          ),
+          /* Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(18),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.center,
+                colors: [
+                  Colors.black.withOpacity(0.6),
+                  Colors.transparent,
+                ],
               ),
             ),
-          ],
-        ),
+          ), */
+          Positioned(
+            top: 10,
+            left: 12,
+            child: buildUsername(context, post),
+          ),
+          // Heart Animation
+          if (shouldAnimateUp || shouldAnimatetDown)
+            Center(
+              child: AnimatedBuilder(
+                animation: _heartAnimationController,
+                builder: (context, _) {
+                  return Icon(
+                    Icons.favorite,
+                    color: couleurBleuClair2,
+                    size: _heartAnimation.value,
+                  );
+                },
+              ),
+            ),
+        ],
       ),
     );
   }
 
-  Widget _buildCard(String imageUrl) {
-    return Container(
-      clipBehavior: Clip
-          .hardEdge, // S'assurer que l'image ne dépasse pas les bordures arrondies
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(18), // Rayon des coins arrondis
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            spreadRadius: 3,
-            blurRadius: 7,
-            offset: const Offset(0, 3),
+  Widget buildUsername(BuildContext context, Post state) {
+    return GestureDetector(
+      onTap: () => _navigateToUserScreen(context, state),
+      child: Row(
+        children: [
+          UserProfileImage(
+            radius: 23,
+            outerCircleRadius: 24,
+            profileImageUrl: state.author.profileImageUrl,
+          ),
+          const SizedBox(width: 12),
+          Text(
+            state.author.username,
+            style: AppTextStyles.titlePost(context),
           ),
         ],
       ),
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
     );
+  }
+
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return Container(
+          height: 200,
+          color: Colors.white,
+          child: const Center(
+            child: Text("Bottom Sheet Content"),
+          ),
+        );
+      },
+    ).then((_) {
+      // Ceci sera appelé lorsque le BottomSheet est complètement fermé
+      _hasShownBottomSheet = false;
+    });
+  }
+
+  void _navigateToUserScreen(BuildContext context, Post state) {
+    GoRouter.of(context)
+        .push('/user/${state.author.id}?username=${state.author.username}');
+  }
+
+  void _navigateToPostScreen(BuildContext context, Post state) {
+    final username = state.author.username;
+    GoRouter.of(context).push('/post/${state.id}?username=$username');
   }
 
   void _changeImage(int swiperNumber) {
@@ -155,4 +295,27 @@ class _SwipeEventState extends State<SwipeEvent> {
       }
     });
   }
+
+  void _loadImages(List<Post> posts) {
+    _imageUrls1.clear();
+    _imageUrls2.clear();
+    _posts1.clear();
+    _posts2.clear();
+
+    bool addToFirstList = true;
+    for (var post in posts) {
+      var imageUrl = post.imageUrl;
+      if (addToFirstList) {
+        _imageUrls1.add(imageUrl);
+        _posts1.add(post);
+      } else {
+        _imageUrls2.add(imageUrl);
+        _posts2.add(post);
+      }
+      addToFirstList = !addToFirstList;
+    }
+  }
+
+  @override
+  bool get wantKeepAlive => true;
 }

c138e490a92d4f59579b376242eacf3fab2eeddc
« Christian » <christianbastide95@gmail.com>
2024-05-18 19:34:15 +0200

    Fix UI

--- lib/screens/profile_edit/screens/edit_username.dart
+++ lib/screens/profile_edit/screens/edit_username.dart
@@ -1,13 +1,10 @@
-// ignore_for_file: library_private_types_in_public_api
-
-import 'package:bootdv2/screens/profile_edit/cubit/edit_profile_cubit.dart';
-import 'package:bootdv2/screens/profile_edit/widgets/error_dialog.dart';
+import 'package:bootdv2/config/configs.dart';
+import 'package:bootdv2/screens/profile_edit/widgets/appbar_title_editprofile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'package:bootdv2/config/configs.dart';
+import 'package:bootdv2/screens/profile_edit/cubit/edit_profile_cubit.dart';
+import 'package:bootdv2/screens/profile_edit/widgets/error_dialog.dart';
 import 'package:bootdv2/screens/profile/bloc/blocs.dart';
-import 'package:bootdv2/screens/profile_edit/widgets/appbar_title_profile.dart';
 
 class EditUsernameScreen extends StatefulWidget {
   final String userId;
@@ -22,6 +19,8 @@ class EditUsernameScreen extends StatefulWidget {
 }
 
 class _EditUsernameScreenState extends State<EditUsernameScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -33,37 +32,100 @@ class _EditUsernameScreenState extends State<EditUsernameScreen> {
   }
 
   @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        appBar: const AppBarProfile(title: "Edit Username"),
-        body: BlocConsumer<EditProfileCubit, EditProfileState>(
-          listener: (context, state) {
-            if (state.status == EditProfileStatus.success) {
-              Navigator.of(context).pop();
-            } else if (state.status == EditProfileStatus.error) {
-              ErrorDialog(content: state.failure.message);
-            }
-          },
-          builder: (context, editState) {
-            return BlocBuilder<ProfileBloc, ProfileState>(
-              builder: (context, profileState) {
+      child: BlocBuilder<ProfileBloc, ProfileState>(
+        builder: (context, profileState) {
+          if (profileState.status == ProfileStatus.loaded &&
+              _usernameController.text.isEmpty) {
+            _usernameController.text = profileState.user.username;
+          }
+
+          return Scaffold(
+            appBar: const AppBarEditProfile(title: "Edit Username"),
+            body: BlocConsumer<EditProfileCubit, EditProfileState>(
+              listener: (context, state) {
+                if (state.status == EditProfileStatus.success) {
+                  Navigator.of(context).pop();
+                } else if (state.status == EditProfileStatus.error) {
+                  ErrorDialog(content: state.failure.message);
+                }
+              },
+              builder: (context, editState) {
                 return SingleChildScrollView(
+                  padding: const EdgeInsets.all(16.0),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      if (editState.status == EditProfileStatus.submitting)
-                        const LinearProgressIndicator(),
-                      Container(
-                        color: red,
-                      )
+                      TextField(
+                        cursorColor: couleurBleuClair2,
+                        style: AppTextStyles.titleSmallGrey(context),
+                        controller: _usernameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Username',
+                          labelStyle: TextStyle(color: Colors.black),
+                          floatingLabelStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          context
+                              .read<EditProfileCubit>()
+                              .usernameChanged(value);
+                        },
+                      ),
+                      const SizedBox(height: 16.0),
+                      Text(
+                        "Aidez les gens à trouver votre compte à l'aide de votre nom le plus connu, que ce soit votre nom complet, votre surnom ou votre nom d'entreprise.\n\nVous pouvez uniquement changer votre nom deux fois tous les 14 jours.",
+                        style: AppTextStyles.bodySmallStyleGrey(context),
+                      ),
+                      const SizedBox(height: 16.0),
                     ],
                   ),
                 );
               },
-            );
-          },
-        ),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton:
+                buildFloatingValidateButton(context, profileState),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildFloatingValidateButton(
+      BuildContext context, ProfileState profileState) {
+    return FloatingActionButton.extended(
+      backgroundColor: couleurBleuClair2,
+      onPressed: () {
+        final currentUsername = _usernameController.text.isEmpty
+            ? profileState.user.username
+            : _usernameController.text;
+        context.read<EditProfileCubit>().usernameChanged(currentUsername);
+        context.read<EditProfileCubit>().submit();
+      },
+      label: Text(
+        AppLocalizations.of(context)!.translate('validate'),
+        style: Theme.of(context)
+            .textTheme
+            .headlineSmall!
+            .copyWith(color: Colors.white),
       ),
     );
   }
